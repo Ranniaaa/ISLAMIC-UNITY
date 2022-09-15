be7448003be15a6e3b66c5fac90ca7586fb6207e
Ranniaaa <97061412+Ranniaaa@users.noreply.github.com>
2022-09-16 00:23:53 +0200

    first commit

---
+++ QURAN/quran.js
@@ -0,0 +1,12 @@
+
+
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+  }
+
+      
